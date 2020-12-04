ee3dc03d46ac8764d2e918d73167676c8ce6527d
felipelopezortega <flopezortega@ucol.mx>
2020-12-03 19:04:42 -0600

    function contarImparesFor added

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ export default class App {
 
         for( let i = 0; i <= 20; i = i + 2 ){
 
-            if(suma%2 === 0){
+            if(suma % 2 === 0){
 
                 suma = suma + i
             }
@@ -16,6 +16,21 @@ export default class App {
         return suma 
     }
 
+    contarImparesFor(inicio, fin){
+       
+        let impares = 0
+
+        for(let i=inicio; i<=fin; i = i + 1){
+
+            if(i!==0){
+
+                impares = impares + 1
+            }
+        }
+
+        return impares / 2
+    }
+
 
 }
 
@@ -23,3 +38,6 @@ let app = new App()
 
 //Función que regresa la suma de los números pares del 0 al 20
 console.log(app.sumarParesFor())
+
+//Función que determina los números impares entre un intervalo
+console.log(app.contarImparesFor(5,10))
