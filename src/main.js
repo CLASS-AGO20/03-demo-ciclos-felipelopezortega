a05c684de540cb4aebb8f1714a36fbb0433a393a
felipelopezortega <flopezortega@ucol.mx>
2020-12-03 18:50:39 -0600

    function sumarParesFor added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-export default class App {}
+export default class App {
 
-let app = new App();
+    sumarParesFor(){
+        
+        let suma = 0
+
+        for( let i = 0; i <= 20; i = i + 2 ){
+
+            if(suma%2 === 0){
+
+                suma = suma + i
+            }
+            
+        }
+
+        return suma 
+    }
+
+
+}
+
+let app = new App()
+
+//Función que regresa la suma de los números pares del 0 al 20
+console.log(app.sumarParesFor())
