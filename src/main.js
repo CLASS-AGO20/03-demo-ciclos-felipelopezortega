2ea418246da2768a6369afd3bac32c6e45283e62
felipelopezortega <flopezortega@ucol.mx>
2020-12-04 00:10:34 -0600

    had a little trouble with function 3 but now it is fixed and tested

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ export default class App {
 
     sumarParesFor(){
         
-        let suma = 0 
-        
+        let suma = 0
+
         for( let i = 0; i <= 20; i = i + 2 ){
 
             if(suma % 2 === 0){
@@ -44,38 +44,24 @@ export default class App {
     sumarParesWhile(){
 
         let i = 1
-
         let suma = 0
 
-        while( i <= 20){
+        while(i<=20){
 
-            if (i % 2 ===0){
+            if(i%2===0){
 
-            suma = suma + 1
+                suma = suma + i
             }
-
+            
+            i = i + 1
         }
 
-        i = i + 1
-
-        return suma
+            return suma
+        
     }
 
-    contarImparesWhile(inicio, fin){
+    
 
-        let impares = 0
-
-        while(inicio <= fin){
-
-            if (inico!==0){
-
-                impares = impares + 1
-            }
-
-            inicio++
-        }
-
-    }
 
 }
 
@@ -88,8 +74,9 @@ console.log(app.sumarParesFor())
 console.log(app.contarImparesFor(5,10))
 console.log(app.contarImparesFor(10,5))
 
-//Función que suma los números pares entre 1 y 20
+//Función que suma los números pares del 1 al 20
 console.log(app.sumarParesWhile())
 
-//Función que determina cuantos números impares hay en un intervalo
-console.log(app.contarImparesWhile(12,20))
+
+
+
